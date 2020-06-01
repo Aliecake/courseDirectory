dc290e8545f04cb78f35f878089e8f160f97eaa0
Aliecake <neyabenz@gmail.com>
2020-06-01 09:09:44 -0400

    paths added

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,19 @@ import {
 } from 'react-router-dom';
 
 import './styles/App.css';
-import Header from './components/Header'
-import Courses from './components/Course/Courses'
-import CourseDetail from './components/Course/CourseDetail'
-import NotFound from './components/NotFound'
+import Header from './components/Header';
+import Courses from './components/Course/Courses';
+import CourseDetail from './components/Course/CourseDetail';
+import NotFound from './components/NotFound';
 import withContext from './Context';
+import UserSignUp from './components/User/UserSignUp';
+import UserSignIn from './components/User/UserSignIn';
 
-const HeaderWithContext = withContext(Header)
-const CoursesWithContext = withContext(Courses)
-const CourseDetailWithContext = withContext(CourseDetail)
+const HeaderWithContext = withContext(Header);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const CoursesWithContext = withContext(Courses);
+const CourseDetailWithContext = withContext(CourseDetail);
 
 
 export default () => (
@@ -24,6 +28,9 @@ export default () => (
       <Switch>
         <Route exact path="/" component={ CoursesWithContext } />
         <Route path="/courses/:id" component={ CourseDetailWithContext }/>
+
+        <Route path="/signup" component={ UserSignUpWithContext }/>
+        <Route path="/signin" component={ UserSignInWithContext }/>
         <Route path="*" component={ NotFound } />
       </Switch>
     </div>
