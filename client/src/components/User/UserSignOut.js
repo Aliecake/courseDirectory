d41467a19d6698f3b18bd03facbed6aaf0c0dee2
Aliecake <neyabenz@gmail.com>
2020-06-04 13:17:32 -0400

    sign out

--- client/src/components/User/UserSignOut.js
+++ client/src/components/User/UserSignOut.js
@@ -1,1 +1,10 @@
-//stateless
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+export default ({context}) => {
+  context.actions.signOut();
+
+  return (
+    <Redirect to="/" />
+  );
+}
