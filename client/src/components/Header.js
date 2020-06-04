a1cc9cc5cee99be41773007d8b8d1667fd9aeab0
Aliecake <neyabenz@gmail.com>
2020-06-04 13:17:01 -0400

    sign in user name on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,16 +13,19 @@ const Header = (props) => (
 
   <div className="header">
     <div className="bounds">
-      <h1 className="header--logo">Course Library</h1>
+      <Link to="/">
+        <h1 className="header--logo">Course Library</h1>
+      </Link>
+      
       <nav>
         {props.context.authenticatedUser ?
           <Fragment>
-            <span>Welcome PERSON</span>
+            <span>Welcome { props.context.authenticatedUser.firstName } !</span>
           <Link className="signout" to="/signout">Sign Out</Link>
           </Fragment>
            : 
            <Fragment>
-            <span>Welcome</span>
+            <span>Welcome, </span>
             <Link className="signup" to="/signup">Sign up</Link>
             <Link className="signin" to="/signin">Sign in</Link>
           </Fragment>
