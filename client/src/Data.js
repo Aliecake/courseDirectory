df9f5e3720a35ae6c33def726ecd715ac8ee45f8
Aliecake <neyabenz@gmail.com>
2020-06-10 14:23:28 -0400

    linter

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,83 +1,87 @@
 import config from './config';
 
 export default class Data {
-   api(path, method, body = null, requiresAuth = false, credentials = null) {
+  api(path, method, body = null, requiresAuth = false, credentials = null) {
     const url = config.apiBaseUrl + path;
 
     const options = {
-        method,
-        headers: {
-            'Content-Type' : 'application/json; charset=utf-8'
-        },
-    }
+      method,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    };
 
     if (body !== null) {
-        options.body = JSON.stringify(body)
+      options.body = JSON.stringify(body);
     }
-    
+
     if (requiresAuth) {
-        const encodedCredentials = btoa(`${credentials.emailAddress}:${credentials.password}`)
+      const encodedCredentials = btoa(
+        `${credentials.emailAddress}:${credentials.password}`
+      );
 
-        options.headers[`Authorization`] = `Basic ${encodedCredentials}`
+      options.headers.Authorization = `Basic ${encodedCredentials}`;
     }
-    return fetch(url, options)
-   }
+    return fetch(url, options);
+  }
 
-   //USER
-   async getUser(emailAddress, password) {
-       console.log(`GET USER CALLED`)
-       const response = await this.api(`/users`, 'GET', null, true, {emailAddress, password});
-       
-       if(response.status === 200) {
-           return response.json().then(data => data)
-       } else if (response.status === 401) {
-          return null
-       } else {
-           throw new Error()
-       }
-   }
+  // USER
+  async getUser(emailAddress, password) {
+    const response = await this.api(`/users`, 'GET', null, true, {
+      emailAddress,
+      password,
+    });
 
+    if (response.status === 200) {
+      return response.json().then(data => data);
+    }
+    if (response.status === 401) {
+      return null;
+    }
+    throw new Error();
+  }
 
-   async createUser(user) {
+  async createUser(user) {
     const response = await this.api(`/users`, 'POST', user);
-    //500 error if user already exists
-    if(response.status === 201) {
-        return []
-    } else if (response.status === 400) {
-        return response.json().then(data => {
-            return data
-        })
-    } else {
-        throw new Error()
+    // 500 error if user already exists
+    if (response.status === 201) {
+      return [];
     }
+    if (response.status === 400) {
+      return response.json().then(data => data);
+    }
+    throw new Error();
+  }
 
-   }
+  // COURSES
+  async getCourses() {
+    const response = await this.api('/courses', 'GET');
+    if (response.status === 200) {
+      console.log(`200 OK - got courses`);
+      return response.json();
+    }
+    console.log(`no courses`);
+  }
 
-   //COURSES
-   async getCourses() {
-       const response = await this.api('/courses', 'GET');
-       if (response.status === 200) {
-           console.log(`200 OK - got courses`)
-           return response.json()
-       } else {
-           console.log(`no courses`)
-       }
-   }
-   async getOneCourse(id) {
-       const response = await this.api(`/courses/${id}`, 'GET');
-       if (response.status === 200) {
-           console.log(`200, got the course`);
-           return response.json()
-       } else {
-           console.log(`course doesnt exist`)
-       }
-   }
-   async handleDelete(id) {
-       const response = await this.api(`/courses/${id}`, 'DELETE')
-       if (response.status === 204) {
-           console.log(`deleted`)
-       } else {
-           console.log(`forbidden`)
-       }
-   }
+  async getOneCourse(id) {
+    const response = await this.api(`/courses/${id}`, 'GET');
+    if (response.status === 200) {
+      return response.json();
+    }
+    console.log(`course doesnt exist`);
+  }
+
+  async handleDelete(id, emailAddress, password) {
+    // PW undefined
+    const response = await this.api(`/courses/${id}`, 'DELETE', null, true, {
+      emailAddress,
+      password,
+    });
+    console.log(response.status);
+    if (response.status === 204) {
+      console.log(`deleted`);
+    } else {
+      console.log(`forbidden`);
+    }
+  }
 }
