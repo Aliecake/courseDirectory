9a3926876effe7b4396a5fd77852782d2c125a51
Aliecake <neyabenz@gmail.com>
2020-06-10 14:22:52 -0400

    cookies for user

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,78 +1,77 @@
 import React, { Component } from 'react';
+import Cookies from 'js-cookie';
 import Data from './Data';
 
-//create context
-const Context = React.createContext(); 
+// create context
+const Context = React.createContext();
 
-//provider component
+// provider component
 export class Provider extends Component {
+  state = {
+    authenticatedUser: Cookies.getJSON('authenticatedUser') || null
+  };
 
-    state = {
+  constructor() {
+    super();
+    this.data = new Data();
+  }
+
+  render() {
+    const { authenticatedUser } = this.state;
+
+    const value = {
+      authenticatedUser,
+      data: this.data,
+      actions: {
+        signIn: this.signIn,
+        signOut: this.signOut,
+      },
+    };
+
+    return (
+      <Context.Provider value={value}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+
+  signIn = async (username, password) => {
+    const user = await this.data.getUser(username, password);
+    console.log(`SIGN IN CALLED`);
+    if (user !== null) {
+      this.setState(() => ({
+        authenticatedUser: user.user,
+      }));
+      Cookies.set('authenticatedUser', JSON.stringify(user), {
+        expires: 1
+      });
+    }
+    console.log(user);
+    return user.user;
+  };
+
+  signOut = () => {
+    this.setState({
       authenticatedUser: null,
-    }
+    });
+    Cookies.remove('authenticatedUser');
+  };
+}
 
-    constructor() {
-      super();
-      this.data = new Data();
-    }
-  
-    render() {
-      const { authenticatedUser } = this.state
+export const { Consumer } = Context;
 
-      const value = {
-        authenticatedUser,
-        data: this.data,
-        actions: {
-          signIn: this.signIn,
-          signOut: this.signOut
-        }
-      };
-      
-      return (
-        <Context.Provider value={ value }>
-          {this.props.children}
-        </Context.Provider>  
-      );
-    }
+/**
+ * A higher-order component that wraps the provided component in a Context Consumer component.
+ * @param {class} Component - A React component.
+ * @returns {function} A higher-order component.
+ */
 
-    signIn = async (username, password) => {
-      const user = await this.data.getUser(username, password);
-      console.log(`SIGN IN CALLED`)
-      if(user !== null){
-        this.setState(() => {
-          return {
-            authenticatedUser: user.user
-          }
-        })
-      }
-      console.log(user)
-      return user.user
-    }
-
-    signOut = () => {
-      this.setState({
-        authenticatedUser: null
-      })
-    }
-
-  }
-
-
-export const Consumer = Context.Consumer;
-  
-  /**
-   * A higher-order component that wraps the provided component in a Context Consumer component.
-   * @param {class} Component - A React component.
-   * @returns {function} A higher-order component.
-   */
-  
-  export default function withContext(Component) {
-    return function ContextComponent(props) {
-      return (
-        <Context.Consumer>
-          {context => <Component {...props} context={context} />}
-        </Context.Consumer>
-      );
-    }
-  }
-  
+export default function withContext(Component) {
+  return function ContextComponent(props) {
+    return (
+      <Context.Consumer>
+        {context => <Component {...props} context={context} />}
+      </Context.Consumer>
+    );
+  };
+}
