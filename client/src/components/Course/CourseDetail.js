d814fc73a1ceeb0fcbcaf60e5d751fd42d07b5ea
Aliecake <neyabenz@gmail.com>
2020-06-10 14:24:29 -0400

    linter

--- client/src/components/Course/CourseDetail.js
+++ client/src/components/Course/CourseDetail.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
-import CourseCard from './CourseCard'
+import React, { Component } from 'react';
+import CourseCard from './CourseCard';
 import NotFound from '../NotFound';
 
 export default class CourseDetail extends Component {
+  state = {
+    course: null,
+  };
 
-    state = {
-        course: null
+  async componentDidMount() {
+    const { context } = this.props;
+    const { id } = this.props.match.params;
+    const course = await context.data.getOneCourse(id);
+
+    if (course) {
+      this.setState({
+        course: course.course,
+      });
+    } else {
+      //
     }
-    
-    async componentDidMount() {
-        const { context } = this.props
-        const id = this.props.match.params.id
-        const course = await context.data.getOneCourse(id)
+  }
 
-        if(course) {
-            
-            this.setState({
-                course: course.course
-            })
-        } else {
-            //
-        }
-
-    }
-
-    render() {
-        return (
-            this.state.course? <div>
-                <CourseCard course={this.state.course} context={this.props}/>
-                </div> : <NotFound noCourse={true}/>
-            
-        )
-    }
+  render() {
+    return this.state.course ? (
+      <div>
+        <CourseCard course={this.state.course} context={this.props} />
+      </div>
+    ) : (
+      <NotFound noCourse />
+    );
+  }
 }
