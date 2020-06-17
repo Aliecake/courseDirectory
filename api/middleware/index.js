2e1bdb7214da36a698973e4622ca8a54eb1c9a27
Aliecake <neyabenz@gmail.com>
2020-06-17 13:07:40 -0400

    removed console log

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -20,7 +20,6 @@ const middleware = {}
   /** Handler function to check user authentication*/
   middleware.authenticateUser = async(req, res, next) => {
 
-    console.log(req)
     let message = null;
     let user = null;
     //parsed from Auth header
