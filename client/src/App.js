465a709782bc86e25e24c28b315f6206dde0c73d
Aliecake <neyabenz@gmail.com>
2020-06-17 13:08:06 -0400

    modifying auth

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,16 @@ import withContext from './Context';
 import UserSignUp from './components/User/UserSignUp';
 import UserSignIn from './components/User/UserSignIn';
 import UserSignOut from './components/User/UserSignOut';
-import Authenticated from './components/User/Authenticated';
+import CreateCourse from './components/Course/CreateCourse';
+
 
 const HeaderWithContext = withContext(Header);
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
 const CoursesWithContext = withContext(Courses);
-const AuthenticatedWithContext = withContext(Authenticated);
 const CourseDetailWithContext = withContext(CourseDetail);
+const CreateCourseWithContext = withContext(CreateCourse)
 
 export default () => (
   <Router>
@@ -27,12 +28,14 @@ export default () => (
       <Switch>
         <Route exact path="/" component={CoursesWithContext} />
         <Route path="/courses/:id" component={CourseDetailWithContext} />
+        <Route path="/create-course" component={CreateCourseWithContext} />
 
         <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
 
-        <Route path="/authenticated" component={AuthenticatedWithContext} />
+
+      
         <Route path="*" component={NotFound} />
       </Switch>
     </div>
