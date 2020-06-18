d24d960f8b9ed6bfcd7eb316f808b869bea430c0
Aliecake <neyabenz@gmail.com>
2020-06-18 12:34:39 -0400

    pass course state through link

--- client/src/components/Course/CourseCard.js
+++ client/src/components/Course/CourseCard.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import Cookies from 'js-cookie';
+
 import ErrorsDisplay from '../ErrorsDisplay';
 
 export default class CourseCard extends Component {
@@ -26,10 +28,12 @@ export default class CourseCard extends Component {
             <div className="grid 100">
               {/* Uses cookies instead of context, so will delete on reload */}
               <span>
-                {/* check if there is an auth user, and if so make sure they match whom added course*/}
-                {context.authenticatedUser && context.authenticatedUser.id === course.addedBy.id ? (
+                {/* check if there is an auth user in either context or cookies, and if so make sure they match whom added course*/}
+                {(context.authenticatedUser || user) && context.authenticatedUser.id === course.addedBy.id ? (
                   <Fragment>
-                    <Link className="button" to={`/courses/${course.id}/update`}>
+                    <Link className="button" to={{
+                      pathname:`/courses/${course.id}/update`, state: {course}
+                      }} >
                       Update Course
                     </Link>
 
@@ -93,14 +97,9 @@ export default class CourseCard extends Component {
                 <li className="course--stats--list--item">
                   <h4>Materials needed</h4>
                   <ul>
-                    {/* remove first asterisk, and replace all other trailing newline asterisks if there are any */}
-                    {course.materialsNeeded
-                      ? course.materialsNeeded
-                          .replace("*", "")
-                          .replace(/\n\*/g, ",")
-                          .split(',')
-                          .map((item, i) => <li key={i}>{item}</li>)
-                      : ''}
+                    <ReactMarkdown>
+                      {course.materialsNeeded}
+                    </ReactMarkdown>
                   </ul>
                 </li>
               </ul>
